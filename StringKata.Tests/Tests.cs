8cdfafec035c21e25cfb6e00a38868039d9362e6
Nicolas Darren Chetty <n.darren.c@hotmail.com>
2020-09-14 09:41:48 +0200

    proj

---
+++ StringKata.Tests/Tests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace StringKata.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Kata GetSUT()
+        {
+            return new Kata();
+        }
+
+        [Test]
+        public void Add_GivenEmptyString_ShouldReturnZero()
+        {
+            // Arrange
+            var input = string.Empty;
+            var expectedResult = 0;
+
+            // Act
+            var actualResult = GetSUT().Add(input);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Add_GivenOneNumber_ShouldReturnSameNumber()
+        {
+            // Arrange
+            var input = "1";
+            var expectedResult = 1;
+
+            // Act
+            var actualResult = GetSUT().Add(input);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ StringKata/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringKata
+{
+    public class Kata
+    {
+        public int Add(string input)
+        {
+            return string.IsNullOrEmpty(input) ? 0 : 1;
+        }
+    }
+}
